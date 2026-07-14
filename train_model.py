a2692d96e2eef94f712e3601d88f0901b894bd1e
Aya Oumaima <ayaoumaima@MacBook-Air-de-Aya.local>
2026-07-14 21:02:57 +0100

    Évaluer le modèle

--- train_model.py
+++ train_model.py
@@ -1,22 +1,61 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
 
 # Charger le dataset
 df = pd.read_csv("diabetes.csv")
 
-print("===== Aperçu des données =====")
-print(df.head())
+# Colonnes à nettoyer
+columns = ["Glucose", "BloodPressure", "SkinThickness", "Insulin", "BMI"]
 
-print("\n===== Dimensions =====")
-print(df.shape)
+# Remplacer les 0 par des valeurs manquantes
+df[columns] = df[columns].replace(0, pd.NA)
 
-print("\n===== Colonnes =====")
-print(df.columns)
+# Remplacer les valeurs manquantes par la médiane
+for col in columns:
+    df[col] = df[col].fillna(df[col].median())
 
-print("\n===== Informations =====")
-print(df.info())
+print("Nettoyage terminé !")
 
-print("\n===== Statistiques =====")
-print(df.describe())
+# ==========================
+# Séparer X et y
+# ==========================
 
-print("\n===== Valeurs manquantes =====")
-print(df.isnull().sum())
+X = df.drop("Outcome", axis=1)
+y = df["Outcome"]
+
+# ==========================
+# Diviser le dataset
+# ==========================
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42
+)
+
+print("Taille de X_train :", X_train.shape)
+print("Taille de X_test :", X_test.shape)
+
+# ==========================
+# Entraîner le modèle
+# ==========================
+
+model = RandomForestClassifier(random_state=42)
+model.fit(X_train, y_train)
+
+# ==========================
+# Faire des prédictions
+# ==========================
+
+predictions = model.predict(X_test)
+
+# ==========================
+# Évaluer le modèle
+# ==========================
+
+accuracy = accuracy_score(y_test, predictions)
+
+print(f"Précision : {accuracy * 100:.2f}%")
