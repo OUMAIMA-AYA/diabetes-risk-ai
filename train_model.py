4f5f654c1243dc4435c7bf9f2eeaee5a2fb8a0a8
Aya Oumaima <ayaoumaima@MacBook-Air-de-Aya.local>
2026-07-15 19:16:54 +0100

    evaluer le modele

--- train_model.py
+++ train_model.py
@@ -2,6 +2,15 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import (
+    accuracy_score,
+    precision_score,
+    recall_score,
+    confusion_matrix,
+    classification_report,
+    ConfusionMatrixDisplay
+)
+import matplotlib.pyplot as plt
 
 # Charger le dataset
 df = pd.read_csv("diabetes.csv")
@@ -57,5 +66,23 @@ predictions = model.predict(X_test)
 # ==========================
 
 accuracy = accuracy_score(y_test, predictions)
+precision = precision_score(y_test, predictions)
+recall = recall_score(y_test, predictions)
 
-print(f"Précision : {accuracy * 100:.2f}%")
+print(f"Accuracy  : {accuracy:.2%}")
+print(f"Precision : {precision:.2%}")
+print(f"Recall    : {recall:.2%}")
+
+# Matrice de confusion
+cm = confusion_matrix(y_test, predictions)
+
+print("\nConfusion Matrix :")
+print(cm)
+
+disp = ConfusionMatrixDisplay(confusion_matrix=cm)
+disp.plot()
+plt.show()
+
+# Rapport complet
+print("\nClassification Report :")
+print(classification_report(y_test, predictions))
